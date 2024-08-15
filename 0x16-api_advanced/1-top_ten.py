7efa19cc310255ed6ce9e7f212bc52fb17da68e6
yosefbenti <yosefbenti61@gmail.com>
2024-08-15 16:29:45 +0300

    t

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -14,7 +14,7 @@ def top_ten(subreddit):
     - If the subreddit is invalid, print None.
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/yourusername)"}
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/yosefbenti)"}
     params = {"limit": 10}
     
     try:
