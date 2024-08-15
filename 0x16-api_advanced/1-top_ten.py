292f819f3a758f6114cbb08eae128e5230b7ec44
yosefbenti <yosefbenti61@gmail.com>
2024-08-15 15:53:20 +0300

    task 1 edited

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit."""
+""" Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit."""
+
+import requests
 
 
 def top_ten(subreddit):
-    """function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit."""
-    import requests
-    import sys
-
+    """ Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit. """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
+
     try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
+        response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for i in range(10):
-                print(children[i].get('data').get('title'))
+            children = response.json().get('data', {}).get('children', [])
+            for i in range(min(10, len(children))):
+                print(children[i].get('data', {}).get('title', "None"))
         else:
             print("None")
-    except Exception:
+    except requests.RequestException as e:
         print("None")
