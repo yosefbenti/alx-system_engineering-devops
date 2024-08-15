7d1f1a01ba4647d5fa0f3601af875b21fd25ce45
yosefbenti <yosefbenti61@gmail.com>
2024-08-15 21:12:06 +0300

    push it

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Solve issue from wp-settings 
+# Fixes bad `phpp` extensions to `php`
 
-exec { 'fix':
+exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
