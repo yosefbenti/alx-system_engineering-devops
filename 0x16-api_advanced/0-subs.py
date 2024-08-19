4903b8323a5809d2889921c54389bd213b227850
yosefbenti <yosefbenti61@gmail.com>
2024-08-19 20:47:35 +0300

    he

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,14 @@ for a given subreddit.
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "python:subreddit.subscriber.count:v1.0 (by /u/yourusername)"}
-    
+    headers = {
+        "User-Agent": "python:subreddit.subscriber.count:v1.0 (by /u/yourusername)"
+    }
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
